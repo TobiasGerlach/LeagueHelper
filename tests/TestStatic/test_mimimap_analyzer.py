7f990ba657b98c31f38fceb7e7a00737541590bd
Tobias Gerlach <me@tobias-gerlach.de>
2022-12-01 22:32:34 +0100

    fixed ALL paths

--- tests/TestStatic/test_mimimap_analyzer.py
+++ tests/TestStatic/test_mimimap_analyzer.py
@@ -12,24 +12,20 @@ class TestMinimapAnalyzer:
     def test_find_circles(self):
         media_path = Path(__file__).parent.parent / "media"
         minimap = cv2.imread(str(media_path / "minimap.png"))
+        # inRange(image, cv::Scalar(0, 125, 0), cv::Scalar(255, 200, 255), output) 90,20,20 30,60,90
         print(minimap)
         assert False
 
     def test_determine_champions(self):
-        minimap = cv2.imread(
-            "C:\\Users\\tobia\\Documents\\Code\\LeagueHelper\\tests\\media\\minimap.png"
-        )
+        media_path = Path(__file__).parent.parent / "media"
+        minimap = cv2.imread(str(media_path / "minimap.png"))
         assert False
 
     def test_tempalte_matching(self):
         ma = MinimapAnalyzer()
-
-        minimap = cv2.imread(
-            "C:\\Users\\tobia\\Documents\\Code\\LeagueHelper\\tests\\media\\minimap.png"
-        )
-        template = cv2.imread(
-            "C:\\Users\\tobia\\Documents\\Code\\LeagueHelper\\media\\blue_circle.png"
-        )
+        media_path = Path(__file__).parent.parent / "media"
+        minimap = cv2.imread(str(media_path / "minimap.png"))
+        template = cv2.imread(str(media_path / "blue_circle.png"))
         ma._tempalte_matching(minimap, template)
 
         assert False
